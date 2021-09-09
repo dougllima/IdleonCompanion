4f29a3d8829407c04b68f704cf16e33ed67eb5e8
Douglas Lima <doug.llima@hotmail.com>
2021-09-09 09:49:52 -0300

    update top text and wiki link

--- src/pages/tools/RefinaryOptmizer.vue
+++ src/pages/tools/RefinaryOptmizer.vue
@@ -1,6 +1,7 @@
 <template>
   <q-banner inline-actions>
-    Use this page to optimize yours refinary levels and get the most of all sault.
+    Use this page to optimize yours refinary levels and get the most of all
+    sault.
     <template v-slot:action>
       <q-btn-dropdown outline label="Wiki">
         <q-list separator>
@@ -13,6 +14,29 @@
       </q-btn-dropdown>
     </template>
   </q-banner>
+  <q-card class="m-4">
+    <q-card-section>
+      <div class="flex justify-between items-center">
+        <div class="text-2xl">Refinary Optmizer</div>
+        <div class="text-sm">Last Updated: {{ lastUpdated }}</div>
+      </div>
+      <div class="flex">
+        <div class="w-full">
+          <div class="w-1/4 md:w-1/6">
+            <q-input
+              v-model.number="cycleTime"
+              type="time"
+              step="1"
+              filled
+              label="Cycle time"
+              stack-label
+              class="p-2"
+            />
+          </div>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script lang="ts">
@@ -21,15 +45,20 @@ import { computed, defineComponent, ref } from "vue";
 import { Assets } from "~/composables/Utilities";
 import ICAsset from "~/components/idleon-companion/IC-Asset.vue";
 
-const wikiLinks = new Map([["Smithing", "https://idleon.info/wiki/Smithing"]]);
+const wikiLinks = new Map([
+  ["Construction", "https://idleon.info/wiki/Construction"],
+]);
 
 export default defineComponent({
   name: "RefinaryOptmizer",
   components: { ICAsset },
   setup() {
+    const lastUpdated = "09/09/2021";
+
     return {
       Assets,
       wikiLinks,
+      lastUpdated,
     };
   },
 });
